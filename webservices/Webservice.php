cae2609b0e4183d0e0204b546b895063d7b14df5
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-21 10:49:12 +0000

    Removing duplicate curl setting

--- webservices/Webservice.php
+++ webservices/Webservice.php
@@ -133,7 +133,6 @@ namespace simple_webservices {
             curl_setopt($curl_handle, CURLOPT_URL, $this->getBaseUrl() . $endpoint);
             curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, $this->getTimeout());
             curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl_handle, CURLOPT_USERAGENT, "Simple-Webservices v" . self::$VERSION . " <https://github.com/mapkyca/simple-webservices>; " . $this->getUserAgent());
             curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, 1);
